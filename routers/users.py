1362fe4c821009b56e959c1127ace2f1688e1c4c
Muneeb Arshad <42831583+mnbrshd@users.noreply.github.com>
2023-11-03 16:18:06 +0500

    Added alembic for data migration support

--- routers/users.py
+++ routers/users.py
@@ -14,7 +14,6 @@ router = APIRouter(
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-
 def get_db():
     db = SessionLocal()
     try:
@@ -22,10 +21,8 @@ def get_db():
     finally:
         db.close()
 
-
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependecy = Annotated[dict, Depends(get_current_user)]
-
 
 class UserVerification(BaseModel):
     password: str
@@ -36,29 +33,35 @@ class UserVerification(BaseModel):
 # async def read_all_users(user: user_dependecy, db: db_dependency):
 #     if user is None:
 #         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
-
+    
 #     return db.query(Users).all()
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_user(user: user_dependecy, db: db_dependency):
     if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
     return db.query(Users).filter(Users.id == user.get('id')).first()
-
 
 @router.put('/password', status_code=status.HTTP_204_NO_CONTENT)
 async def change_password(user: user_dependecy, db: db_dependency, user_verification: UserVerification):
     if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
+    
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    user_model.hashed_password = bcrypt_context.hash(user_verification.password)
+
+    db.add(user_model)
+    db.commit()
+
+@router.put('/phone_number/{phone_number}', status_code=status.HTTP_204_NO_CONTENT)
+async def update_phone_number(user: user_dependecy, db: db_dependency, phone_number: str):
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication Failed')
 
     user_model = db.query(Users).filter(Users.id == user.get('id')).first()
 
-    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail='Error on password change')
-    user_model.hashed_password = bcrypt_context.hash(
-        user_verification.new_password)
+    user_model.phone_number = phone_number
+
     db.add(user_model)
     db.commit()
